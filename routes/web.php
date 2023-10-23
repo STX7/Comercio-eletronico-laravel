6ede41594b7aeb28742b262264ae22108b6ef4e4
Samuel da Silva <samueldez2014@outlook.com>
2023-10-23 14:35:30 -0300

    Rota Welcome

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/contato', function () {
     return view('ecommerce.contact');
 });
